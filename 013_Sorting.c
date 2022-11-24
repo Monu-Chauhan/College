504eecf9c43c54e034181f8109575bb5ae9a671b
Monu-Chauhan <mc1377929@gmail.com>
2022-11-24 19:53:18 +0530

    Add Insertion Sort

--- 013_Sorting.c
+++ 013_Sorting.c
@@ -33,12 +33,27 @@ void SelectionSort(int *arr,int size)
     }
 
 }
+void insertionSort(int *arr,int size)
+{
+    for(int i=0;i<size;i++)
+    {
+       int key = arr[i];
+       int j=i-1;
+       while(key < arr[j]&& j>=0)
+       {
+        arr[j+1]=arr[j];
+        j--;
+       }
+       arr[j+1]=key;
+    }
+}
 
 int main()
 {
     int arr[10]={30,11,32,23,94,75,66,7,38,19};
     //bubbleSort(arr,10);
     //SelectionSort(arr,10);
+    insertionSort(arr,10);
     for(int i=0;i<10;i++)
     {
         printf("%d ",arr[i]);
